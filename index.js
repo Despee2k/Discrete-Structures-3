92d0ca9491a28bc6dd03909a02f8b7d8fd4e4f6b
Dempsey <dagantareynat@gmail.com>
2024-11-08 21:20:14 +0800

    Added submit image functionality

--- index.js
+++ index.js
@@ -1,49 +1,42 @@
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
-const image = new Image();
-image.crossOrigin = "anonymous"
-
-// Credits to Chraecker on pixabay
-image.src = "owl.png";
-
-const imageSubmit = () => {
-    const matrix = document.getElementById("matrix");
-    matrix.innerHTML = "";
-
-    if(image.complete){
-        for(let i = 0; i < 100; i++){
-            let x = (i % 10) * 10;
-            let y = Math.floor(i / 10) * 10;
-
-            let pixel = context.getImageData(x,y,1,1)
-            let data = pixel.data;
-            let rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
-
-            let cell = document.createElement("div");
-            cell.textContent = rgba;
-            cell.className = "cell";
-            matrix.appendChild(cell);
-        }
-    } else {
-        image.addEventListener("load", () => {
-            imageSubmit();
-        })
+document.getElementById("image-submit").onchange = (e) => {
+    const file = e.target.files[0];
+    if(file){
+        let image = new Image();
+        image.onload = () => draw(image);
+        image.onerror = failed;
+        image.src = URL.createObjectURL(file);
     }
 }
 
-if(canvas.getContext){
-    image.onload = () => {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.drawImage(image, 0, 0, 100, 100);
-        context.beginPath();
-        
-    }
+const draw = (image) => {
+    canvas.width = 100;
+    canvas.height = 100;
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(image, 0, 0, canvas.width, canvas.height);
+}
 
-    if (image.complete) {
-        image.onload();
-    }
+const failed = () => {
+    console.error("The provided file couldn't be loaded");
+}
 
-} else {
-    alert("BROWSER DOES NOT SUPPORT CANVAS!");
+const displayMatrix = () => {
+    const matrix = document.getElementById("matrix");
+    matrix.innerHTML = "";
+
+    for(let i = 0; i < 100; i++){
+        let x = (i % 10) * 10;
+        let y = Math.floor(i / 10) * 10;
+
+        let pixel = context.getImageData(x,y,1,1)
+        let data = pixel.data;
+        let rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
+
+        let cell = document.createElement("div");
+        cell.textContent = rgba;
+        cell.className = "cell";
+        matrix.appendChild(cell);
+    }
 }
